00dc60210843e6189eba697b6707532ea8630de0
Paul <brodmapa@web.de>
2024-12-16 09:48:45 +0100

    Two Page fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,19 @@
 import streamlit as st
-from home import show as show_home
-from about import show as show_about
-from contact import show as show_contact
+from pages.home import show as show_home
+from pages.about import show as show_about
+from pages.Database import show as show_contact
 
 # Set page configuration
 st.set_page_config(page_title="Portfolio Paul Brodmann", layout="wide")
 
 # Sidebar navigation
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "About", "Contact"])
+page = st.sidebar.radio("Go to", ["Home", "About", "Database"])
 
 # Show the corresponding page
 if page == "Home":
     show_home()
 elif page == "About":
     show_about()
-elif page == "Contact":
+elif page == "Database":
     show_contact()
